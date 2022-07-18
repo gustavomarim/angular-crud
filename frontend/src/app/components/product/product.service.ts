bbd4009fc568c78642545b7a36030ee6e7bc3673
unknown <gustavo.dantas.marim@outlook.com>
2022-07-18 08:58:37 -0300

    feat(readById()/update()): add metodos de por id | atualização de dados por id - readById(): realiza uma requisição GET na API em um único produto; - update(): realiza uma requisição PUT na API, atualizando um único produto.

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -12,6 +12,9 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
+  // Configura a caixa de texto de exibição
+  // de mensagem do snackBar exbido ao executar
+  // uma ação de criação de produtos.
   showMensage(msg: string): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
@@ -30,5 +33,19 @@ export class ProductService {
   // Observable recebe um array de produtos
   read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.BASE_API_URL);
+  }
+
+  // Serviço que obtém produto por ID
+  // Este método é utilizado p/ possibilitar o acesso
+  // aos dados que serão modificados
+  readById(id: string): Observable<Product> {
+    const url = `${this.BASE_API_URL}/${id}`;
+    return this.http.get<Product>(url);
+  }
+
+  // Serviço que atualiza o produto a partir de seu ID
+  update(product: Product): Observable<Product> {
+    const url = `${this.BASE_API_URL}/${product.id}`;
+    return this.http.put<Product>(url, product);
   }
 }
